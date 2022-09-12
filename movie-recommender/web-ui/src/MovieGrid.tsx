faaee3f7d66205b92392e0e26e0dfc7a92b68a80
Harsh Upadhyay (TECH) <haupadhy@microsoft.com>
2022-09-12 00:51:42 -0700

    minor refactor

--- movie-recommender/web-ui/src/MovieGrid.tsx
+++ movie-recommender/web-ui/src/MovieGrid.tsx
@@ -2,13 +2,18 @@ import * as React from "react";
 import Col from "react-bootstrap/esm/Col";
 import Row from "react-bootstrap/esm/Row";
 import { MovieCard } from "./MovieCard";
+import { pick10Random } from "./movieData";
 
 interface MovieGridProps {
-  movies: [string, { movieTitle: string; moviePosterLink: string }][];
+  ratings: { [K in string]: number | null };
 }
 export const MovieGrid: React.FunctionComponent<MovieGridProps> = ({
-  movies,
+  ratings,
 }: MovieGridProps) => {
+  // TODO: movies should come from network call that takes in ratings and returns recommendations
+  const movies = React.useMemo(pick10Random, []);
+
+  // TODO: display some spinner to indicate loading
   return (
     <Row xs={1} md={4} className="g-4">
       {movies.map((item) => {
