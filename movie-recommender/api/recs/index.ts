0d6f841aeacb456a039abc259d1c5077ce7d4922
Harsh Upadhyay (TECH) <haupadhy@microsoft.com>
2022-10-10 18:37:30 -0700

    more error handling

--- movie-recommender/api/recs/index.ts
+++ movie-recommender/api/recs/index.ts
@@ -6,6 +6,11 @@ const api_key = "me0FL322gQVsGZjHM1SodZDsKp34BHvk";
 
 type RatingsCollection = { [K in string]: number };
 
+function getErrorMessage(error: unknown) {
+    if (error instanceof Error) return error.message
+    return String(error)
+}
+
 function isRatingsCollection(arg: any): arg is RatingsCollection {
     return Object.entries(arg).every(entry => typeof (entry[0]) === "string" && typeof (entry[1]) === "number");
 }
@@ -14,7 +19,7 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
     context.log('HTTP trigger function processed a request.');
     try {
         const ratings = JSON.parse(req.body || {})
-        if(!isRatingsCollection(ratings)){
+        if (!isRatingsCollection(ratings)) {
             throw new Error("Malformed data - supplied data is not a collection of movie ratings.");
         }
         context.res = await axios.get(url, {
@@ -26,12 +31,12 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
                 'azureml-model-deployment': 'cors-3'
             }
         });
-    } catch(e){
+    } catch (err) {
         context.res = {
             status: 400,
-            body: e
+            body: {message: getErrorMessage(err)}
         }
     }
- };
+};
 
 export default httpTrigger;
